137974c487bf58932ebe9e99cbe76def04185887
santiagoGuastavino <san.guasta@gmail.com>
2021-04-23 20:52:11 -0300

    formulario de registro

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const pathPublic = path.resolve('public');
 app.use(express.static('public'));
 
-const pathHome = path.resolve('./views/home.html');
+const pathHome = path.resolve('views/index.html');
 app.get('/', (req,res) => res.sendFile(pathHome));
 
-app.listen(3000, () => console.log('Server running'));
+const pathRegister = path.resolve('views/registro.html');
+app.get('/registro', (req,res) => res.sendFile(pathRegister));
+
+app.listen(3001, () => console.log('Server running: 3001'));
