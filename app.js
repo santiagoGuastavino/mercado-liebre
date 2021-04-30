0d854ca65394b5ae702b17f91bc35a4e4f47380f
santiagoGuastavino <san.guasta@gmail.com>
2021-04-30 14:14:14 -0300

    línea para heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const pathPublic = path.resolve('public');
 app.use(express.static('public'));
 
-const pathHome = path.resolve('views/index.html');
+const pathHome = path.resolve('views/home.html');
 app.get('/', (req,res) => res.sendFile(pathHome));
 
 const pathRegister = path.resolve('views/register.html');
@@ -14,4 +14,7 @@ app.get('/registro', (req,res) => res.sendFile(pathRegister));
 const pathLogin = path.resolve('views/login.html');
 app.get('/ingresa', (req,res) => res.sendFile(pathLogin));
 
-app.listen(3001, () => console.log('Server running: 3001'));
+// app.listen(3000, () => console.log('Servidor corriendo: 3000'));
+
+// Para Heroku
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
