2a8dca96912e1a095289a90bd92b6b0b0e337c8e
santiagoGuastavino <san.guasta@gmail.com>
2021-05-15 21:29:46 -0300

    search bar kinda functional

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -19,6 +19,19 @@ let mainController = {
         };
         res.render('index',{'title':title,'articles':articles,'relevantList':relevantList,'inOfferList':inOfferList});
     },
+
+    search: (req,res) => {
+        let title = 'Resultados de búsqueda';
+        let searchResults = [];
+        let searchInput = req.query.search;
+        for (i = 0 ; i < articles.length ; i++) {
+            if (articles[i].name.includes(searchInput)) {
+                searchResults.push(articles[i]);
+            }
+        }
+
+        res.render('./products/product-results',{'title':title,'searchResults':searchResults});
+    }
 }
 
 module.exports = mainController;
--- src/routes/main.js
+++ src/routes/main.js
@@ -3,5 +3,6 @@ let router = express.Router();
 let mainController = require('../controllers/mainController');
 
 router.get('/', mainController.renderHome);
+router.get('/search', mainController.search);
 
 module.exports = router;
